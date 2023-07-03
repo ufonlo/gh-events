fb1bef28d2a69c19b6027069dc55ba5042aa9a5f
ufonlo <socufo@gmail.com>
2023-07-03 20:01:39 +0300

    add comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+// My add comment
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
